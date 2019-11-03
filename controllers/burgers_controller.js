652b5461d48580f96d5b47c989d0eb05ace3e164
ded-nugent <maxwellwesterberg1995@gmail.com>
2019-11-02 19:47:50 -0500

    created .gitignore

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,40 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  burger.all(function(data) {
+    var object = {
+      burgers: data
+    };
+    res.render("index", object);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, false
+  ], function() {
+    res.redirect("/burgers");
+  });
+});
+
+router.post("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(data) {
+    res.redirect("/burgers");
+  });
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('horse_burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('dog_burger', false);
